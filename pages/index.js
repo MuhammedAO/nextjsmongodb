190e2f035983dd0b5d6b4cad538ea89ef9789503
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-08-01 14:04:01 +0100

    dbConnect

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Head from '../components/head';
+import Nav from '../components/nav';
+
+export default () => (
+  <div>
+    <Head title="Home" />
+    <Nav />
+    </div>
+);
---
+++ pages/test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Head from '../components/head'
+
+const test = () => {
+  return (
+    <div>
+    <Head title="Test Page" />
+      <h1>Test Page</h1>
+    </div>
+  )
+}
+
+export default test
