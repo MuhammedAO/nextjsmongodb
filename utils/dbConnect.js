2ece97929cdc81a3342cf8d1d368dd018a798435
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-08-17 21:36:21 +0100

    update dbConnect

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -7,6 +7,9 @@ if (connection.isConnected) {
   return
 } 
  const db = await mongoose.connect(process.env.MONGO_URI, {
+   DB_NAME: process.env.DB_NAME,
+   DB_USER: process.env.DB_USER,
+   DB_PASS: process.env.DB_PASS,
    useNewUrlParser: true,
    useUnifiedTopology: true
  })
