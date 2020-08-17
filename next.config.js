400a43f2651d62e8dd3b00e5f0e60e2c108e3d42
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-08-17 13:19:00 +0100

    next-config

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  webpack: (config) => {
+    config.node = {
+      fs: 'empty'
+    }
+    return config
+  },
+  env: {
+    MONGO_URI:"mongodb+srv://mo:muhammed@cluster0.cti3a.mongodb.net/<dbname>?retryWrites=true&w=majority"
+  },
+  target: 'serverless'
+};
