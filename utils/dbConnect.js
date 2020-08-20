c2b75eeff65ad6384c0842cd6921ecfa082b2934
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-08-20 12:47:05 +0100

    config vars

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -2,14 +2,12 @@ import mongoose from 'mongoose'
 
 const connection = {}
 
+//connect
 const dbConnect = async () => {
 if (connection.isConnected) {
   return
 } 
  const db = await mongoose.connect(process.env.MONGO_URI, {
-   dbName: process.env.DB_NAME,
-   user: process.env.DB_USER,
-   pass: process.env.DB_PASS,
    useNewUrlParser: true,
    useUnifiedTopology: true
  })
